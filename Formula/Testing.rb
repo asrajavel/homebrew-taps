bdd0993a0940a4ba95773826344a63a4f8c003b1
asrajavel <asrajavel267@gmail.com>
2024-07-17 23:02:47 +0530

    fix name

--- Formula/Testing.rb
+++ Formula/Testing.rb
@@ -1,4 +1,4 @@
-class CreateTestingFile < Formula
+class Testing < Formula
   desc "Create a Testing.txt file in the user's home directory"
   homepage "https://github.com/asrajavel/taps"
   url "file:///dev/null"
